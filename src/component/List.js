212c0e1b1bf0c840218d3f648b7638f06df639cc
yeonsu <kimys9295@naver.com>
2023-09-07 16:21:32 +0900

    home데이터 나타내기

--- src/component/List.js
+++ src/component/List.js
@@ -2,35 +2,39 @@ import '../Home/Home.css'
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 
+
 function List() {
 
     const [data, setData] = useState([]);
-    // const params = {
-    //     page: 1
-    //   };
+    const params = {
+        page: 1
+      };
 
   useEffect(() => {
     // https://vercel-express-pied-kappa.vercel.app/qna?page=1
-    axios.get("https://vercel-express-pied-kappa.vercel.app/qna${params}",
-    {params :{
-        page: 1
-      }})
+    // {params :{
+    //   page: 1
+    // }}
+    axios.get(`https://vercel-express-pied-kappa.vercel.app/qna/${params.page}`
+   )
       .then(response => {
-        setData(response.data);
-        console.log(response.data);
+        setData(response.data.data);
+        console.log(response.data.data);
+        
       })
       .catch(error => {
         console.error('Error fetching data:', error);
       });
   }, []);
+  console.log(data.id);
 
   return (
     <div>
       <div className='list'>
-        <div className='list-num'>1</div>
-        <div className='list-title'>이건 뭘까요</div>
-        <div className='list-date'>2023-06-08</div>
-        <div className='list-check'>답변완료</div>
+        <div className='list-num'>{data.id}</div>
+        <div className='list-title'>{data.title}</div>
+        <div className='list-date'>{data.createdAt}</div>
+        <div className='list-check'>{data.isDone}</div>
       </div>
     </div>
   )
